aa18104bb04c937d0174dc1e36f614b3c701dc76
eligarlo <eligarlo.developer@gmail.com>
2022-02-06 19:28:36 +0200

    Add leaflet map

--- src/pages/Listing.tsx
+++ src/pages/Listing.tsx
@@ -3,6 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { db } from 'firebase.config'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import Spinner from 'components/Spinner'
 import shareIcon from 'assets/svg/shareIcon.svg'
 import { IListing } from 'utils/SharedUtils'
@@ -39,6 +40,8 @@ const Listing: React.FC = () => {
     }, 2000)
   }
 
+  const attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+
   if (loading) {
     return <Spinner />
   }
@@ -53,7 +56,7 @@ const Listing: React.FC = () => {
 
       <div className='listingDetails'>
         <p className='listingName'>
-          ${listing.name} -{' '}
+          {listing.name} - $
           {listing.offer
             ? listing.discountedPrice && formatPriceNumber(listing.discountedPrice)
             : formatPriceNumber(listing.regularPrice)}
@@ -78,7 +81,23 @@ const Listing: React.FC = () => {
 
         <p className='listingLocationTitle'>Location</p>
 
-        {/* MAP */}
+        <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: '100%', width: '100%' }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution={attribution}
+              url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            />
+
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link

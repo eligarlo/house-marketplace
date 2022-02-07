1d7c2e05bc490431d6de0e1b694a2f2cd8bd3be5
eligarlo <eligarlo.developer@gmail.com>
2022-02-07 04:37:14 +0200

    Show listings on profile and delete listings

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { getAuth, updateProfile } from 'firebase/auth'
-import { updateDoc, doc } from 'firebase/firestore'
+import {
+  updateDoc,
+  doc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from 'firebase/firestore'
 import { db } from 'firebase.config'
 import { toast } from 'react-toastify'
-import { IAuthUser } from 'utils/SharedUtils'
+import { IAuthUser, IListing } from 'utils/SharedUtils'
 import arrowRight from 'assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from 'assets/svg/homeIcon.svg'
+import Spinner from 'components/Spinner'
+import ListingItem from 'components/ListingItem'
 
 const Profile: React.FC = () => {
   const auth = getAuth()
 
+  const [listings, setListings] = useState<IListing[]>([] as IListing[])
+  const [loading, setLoading] = useState<boolean>(true)
   const [changeDetails, setChangeDetails] = useState<boolean>(false)
   const [formData, setFormData] = useState<IAuthUser>({
     name: auth.currentUser?.displayName == null ? undefined : auth.currentUser?.displayName,
@@ -20,6 +33,34 @@ const Profile: React.FC = () => {
   const { name, email } = formData
 
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchUserListings = async () => {
+      const listingsRef = collection(db, 'listings')
+      const q = query(
+        listingsRef,
+        where('userRef', '==', auth.currentUser?.uid),
+        orderBy('timestamp', 'desc')
+      )
+
+      const querySnap = await getDocs(q)
+
+      let listings = [] as IListing[]
+
+      querySnap.forEach(doc => {
+        const data = doc.data() as IListing
+        return listings.push({
+          id: doc.id,
+          ...data,
+        })
+      })
+
+      setListings(listings)
+      setLoading(false)
+    }
+
+    fetchUserListings()
+  }, [auth.currentUser?.uid])
 
   const handleOnLogout = (): void => {
     auth.signOut()
@@ -58,6 +99,23 @@ const Profile: React.FC = () => {
     }))
   }
 
+  const handleOnDelete = async (listingId: string): Promise<void> => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setLoading(true)
+      const docRef = doc(db, 'listings', listingId)
+      await deleteDoc(docRef)
+
+      const updatedListings = listings.filter(listing => listing.id !== listingId)
+      setListings(updatedListings)
+      setLoading(false)
+      toast.success('Successfully deleted listing')
+    }
+  }
+
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
     <div className='profile'>
       <header className='profileHeader'>
@@ -94,6 +152,17 @@ const Profile: React.FC = () => {
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt='arrow right' />
         </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className='listingText'>Your listings</p>
+            <ul className='listingsList'>
+              {listings.map(listing => (
+                <ListingItem key={listing.id} listing={listing} onDelete={handleOnDelete} />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   )
